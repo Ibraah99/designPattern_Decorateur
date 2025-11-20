8ba1ca77d36269ebfa8c644f072fc18de5f764db
Ibraah99 <ibrahimsogodogo10@gmail.com>
2025-11-20 17:28:00 -0500

    CrypteurXOR pour crypter le message en byte

--- src/App.java
+++ src/App.java
@@ -17,6 +17,20 @@ public class App {
 		Message messageRot1 = new RotateurDeLettre(doubleSecretInverse);
 		System.out.println(messageRot1.getTexte());
 		
+		Message secret1 = new Message("Je connais l\'identite du flapacha !");
+		System.out.println(secret1.getTexte());
+		
+		
+		Message messageCrypte = new CrypteurXOR (secret1);
+		System.out.println(messageCrypte.getTexte());
+		
+		Message messageDecrypte = new CrypteurXOR (messageCrypte);
+		System.out.println(messageDecrypte.getTexte());
+		
+		//Message messageChiffre = new ChiffreurMessage (secret1);
+		//System.out.println(messageChiffre.getTexte());
+		
+		
 	}
 
 }
